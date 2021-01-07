727d9b1ad59da3bd562099cd76ce884f7f6413a6
flaviodiminuto <flavio.diminuto@outlook.com>
2021-01-07 02:39:38 -0300

    [visual] Destacar números Acertados em cada Sequencia
    Destacar cada número acertado em cada sequencia na tela "Verificar Jogo"

--- app/src/test/java/com/flavio/android/megasena/adapter/JogoAdapterTest.java
+++ app/src/test/java/com/flavio/android/megasena/adapter/JogoAdapterTest.java
@@ -1,6 +1,8 @@
 package com.flavio.android.megasena.adapter;
 
+import com.flavio.android.megasena.Modelos.Aposta;
 import com.flavio.android.megasena.Modelos.Sequencia;
+import com.flavio.android.megasena.Modelos.Validacao;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -39,5 +41,29 @@ public class JogoAdapterTest {
         int quantidadeDeLinhas = adapter.quantidadeLinhas(15);
 
         Assert.assertEquals(3, quantidadeDeLinhas);
+    }
+
+    @Test
+    public void numerosSorteados(){
+        Aposta aposta = new Aposta();
+        int[] numeros = {1,2,3,4,5,6};
+        aposta.adicionaSequencia(numeros);
+        JogosAdapter adapter = new JogosAdapter(aposta.getSequencias());
+        Validacao.setNumerosSorteados(numeros);
+        int i = 0;
+        while (++i < 7) {
+            Assert.assertEquals(true, adapter.isNumeroSorteado(i));
+        }
+    }
+
+    @Test
+    public void numerosNaoSorteados(){
+        Aposta aposta = new Aposta();
+        int[] numeros = {1,2,3,4,5,6};
+        aposta.adicionaSequencia(numeros);
+        JogosAdapter adapter = new JogosAdapter(aposta.getSequencias());
+        Validacao.setNumerosSorteados(numeros);
+        Assert.assertEquals(false, adapter.isNumeroSorteado(7));
+        Assert.assertEquals(false, adapter.isNumeroSorteado(8));
     }
 }
